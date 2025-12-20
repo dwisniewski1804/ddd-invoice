4a88f90c4adfa273000b52fa7b9cb38b9844fe05
damian.wisniewski1804@gmail.com <damian.wisniewski1804@gmail.com>
2025-12-20 21:50:28 +0100

    feat(domain): add InvoiceRepository interface

---
+++ src/Modules/Invoices/Domain/Repositories/InvoiceRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Invoices\Domain\Repositories;
+
+use Modules\Invoices\Domain\Entities\Invoice;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * Repository interface for invoice persistence.
+ * 
+ * This interface belongs to the domain layer and defines
+ * the contract for persisting and retrieving invoices.
+ */
+interface InvoiceRepository
+{
+    public function save(Invoice $invoice): void;
+
+    public function findById(UuidInterface $id): ?Invoice;
+
+    /**
+     * @return Invoice[]
+     */
+    public function findAll(): array;
+}
+
