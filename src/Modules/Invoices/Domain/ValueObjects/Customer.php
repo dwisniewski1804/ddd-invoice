828fb9512be7a0a23fcc2b5570addcc8fae96175
damian.wisniewski1804@gmail.com <damian.wisniewski1804@gmail.com>
2025-12-20 21:50:19 +0100

    feat(domain): add Customer value object

---
+++ src/Modules/Invoices/Domain/ValueObjects/Customer.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Invoices\Domain\ValueObjects;
+
+/**
+ * Customer value object.
+ * 
+ * Represents customer information (name and email).
+ */
+final class Customer
+{
+    public function __construct(
+        public string $name,
+        public Email $email,
+    ) {}
+}
+
